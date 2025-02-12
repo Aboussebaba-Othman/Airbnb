f1915c064d863e89df5f8b540be2fc69080ef330
Dilaly <ismaildilali@gmail.com>
2025-02-12 10:37:07 +0100

    changer la template de page annonces

--- config/routes.php
+++ config/routes.php
@@ -3,10 +3,9 @@ use Core\Router;
 
 Router::add('/', 'HomeController', 'index');
 Router::add('/login', 'AuthController', 'login');
-Router::add('/reserver', 'ReservationController', 'reserver');
-Router::add('/payer', 'ReservationController', 'payer');
 Router::add('/annonces', 'AnnonceController', 'index');
-Router::add('/reservation', 'ReservationController', 'details');
+Router::add('/reservation/{id}', 'ReservationController', 'show');
+Router::add('/reservation/book', 'ReservationController', 'book');
 
 
 
