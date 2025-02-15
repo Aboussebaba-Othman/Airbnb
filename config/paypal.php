258413ec202fafc31654b944002bd4cf10cacecd
Dilaly <ismaildilali@gmail.com>
2025-02-15 18:16:52 +0100

    page home et fixing error

--- config/paypal.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'client_id' => 'Aalf959gZW4hB3XcCv53HJIGH-03Vzai9ZCgtemgI7-zNimqzGOTpUBH65jGm52f3kLPmfGZOLTIVIyi',
-    'client_secret' => 'ENrVzt-_PlBDHG1sMkrzhDEImaXNbtnikfUdnh1l1saM5lhF5LTFz4V9UjTN4gQO06igcFmHVWdMQcX5',
-    'mode' => 'sandbox', // Utilisez 'live' pour la production
-];
